03372d8d68f68ba87a2b0ef87337a97f88f04e5f
Tim Ruffles <timruffles@googlemail.com>
2013-07-27 12:54:45 +0100

    niceified

--- public/app.js
+++ public/app.js
@@ -8,6 +8,12 @@ app.controller("botControl",function($scope) {
     $scope.commands.push({ type: "move", path: cmd })
     $scope.$digest()
   }
+  $scope.removeCommand = function(cmd) {
+    $scope.commands = $scope.commands.filter(function(c) {
+      return c != cmd;
+    })
+  }
+  console.log($scope)
 })
 
 app.controller("commands",function($scope) {
@@ -15,14 +21,17 @@ app.controller("commands",function($scope) {
 
 app.directive("commandCanvas",function() {
   return function link(scope,el,attrs) {
-    var command;
+    var command = [];
     var width;
     var height;
+    var fading = false;
 
     ctx = el[0].getContext("2d")
     el.bind("mousedown",lineStart)
     el.bind("mouseup",lineEnd)
-    ctx.strokeStyle = "red"
+    el.bind("mouseout",lineEnd)
+    ctx.strokeStyle = "blue"
+    ctx.lineWidth = 4
 
     function coordsToRatios(points) {
       return points.map(function(p) {
@@ -32,7 +41,18 @@ app.directive("commandCanvas",function() {
     function normaliseCoords(evt) {
       return {x: evt.offsetX, y: evt.offsetY}
     }
+    function fadeOut() {
+      if(fading) return
+      fading = true
+      el.addClass("fade")
+      setTimeout(function() {
+        ctx.clearRect(0,0,width,height)
+        el.removeClass("fade")
+        fading = false
+      },500)
+    }
     function lineStart(evt) {
+      if(fading) return;
       var box = el[0].getBoundingClientRect()
       width = box.width
       height = box.height
@@ -44,19 +64,25 @@ app.directive("commandCanvas",function() {
       $(evt.target).bind("mousemove",lineMove)
     }
     function lineMove(evt) {
+      if(fading) return
       var normed = normaliseCoords(evt)
       ctx.lineTo(normed.x,normed.y)
       command.push(normed)
       ctx.stroke()
     }
     function lineEnd(evt) {
+      if(fading) return
       ctx.stroke();
-      commandFinished(commandFinished)
+      commandFinished()
       $(evt.target).unbind("mousemove",lineMove)
+      command = []
     }
     function commandFinished() {
+      if(fading || command.length < 2) return
+      fadeOut();
       scope.command = coordsToRatios(command)
       scope.$eval( attrs.onpath )
+      command = []
     }
   }
 })
@@ -67,7 +93,7 @@ app.directive("minimap",function() {
   var height;
 
   function drawPath(ctx,points) {
-    ctx.strokeStyle = "red"
+    ctx.strokeStyle = "blue"
     ctx.beginPath()
     points.forEach(function(p,i) {
       ctx[i === 0 ? "moveTo" : "lineTo"](p.x * width,p.y * height)
